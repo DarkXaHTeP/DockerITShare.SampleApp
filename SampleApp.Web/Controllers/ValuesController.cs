b3a48508512b6dda79c1e03f9a4631c046b5f173
Volodymyr Osmak <darkxahtep@gmail.com>
2019-03-20 23:19:40 +0200

    Updated target framework

--- SampleApp.Web/Controllers/ValuesController.cs
+++ SampleApp.Web/Controllers/ValuesController.cs
@@ -1,44 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace SampleApp.Web.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/values")]
     public class ValuesController : Controller
     {
-        // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] {"value1", "value2"};
+            return Ok(new int[] { 1, 2, 3, 4 });
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] int value)
         {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return this.Ok(value);
         }
     }
 }
