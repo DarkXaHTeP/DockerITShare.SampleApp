57c0ba78d18325fac831ba42f47bcb1af0872af2
Volodymyr Osmak <darkxahtep@gmail.com>
2019-03-21 00:09:44 +0200

    Added front-end controller and static files middleware

---
+++ SampleApp.Web/Controllers/FrontEndController.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+
+namespace SampleApp.Web.Controllers
+{
+    public class FrontEndController : Controller
+    {
+        private readonly IHostingEnvironment _env;
+
+        public FrontEndController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+        
+        [Route("{*url}")]
+        public IActionResult Index()
+        {
+            IFileInfo file = _env.WebRootFileProvider.GetFileInfo("index.html");
+
+            if (!file.Exists)
+            {
+                return NotFound("index.html file is not found");
+            }
+            
+            using (var stream = file.CreateReadStream())
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    return Content(reader.ReadToEnd(), "text/html");
+                }
+            }
+        }
+    }
+}
--- SampleApp.Web/Startup.cs
+++ SampleApp.Web/Startup.cs
@@ -25,6 +25,7 @@ namespace SampleApp.Web
         {
                 app
                     .UseDeveloperExceptionPage()
+                    .UseStaticFiles()
                     .UseMvc();
         }
     }
