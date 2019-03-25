a7c01e46e48f4498a984d875182fab6c658f2543
Volodymyr Osmak <darkxahtep@gmail.com>
2019-03-26 01:48:59 +0200

    Implemented draft version of the word controller

---
+++ Controllers/WordController.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SampleApp.Web.DAL;
+
+namespace SampleApp.Web.Controllers
+{
+    [Route("api/words")]
+    public class WordController : Controller
+    {
+        private readonly WordContext _db;
+
+        public WordController(WordContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_db.Words.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            _db.Words.Add(new Word() {Value = value});
+            return this.Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var word = _db.Words.FirstOrDefault(w => w.Id == id);
+            if (word == null)
+            {
+                return NotFound();
+            }
+
+            _db.Words.Remove(word);
+            return Ok();
+        }
+    }
+}
